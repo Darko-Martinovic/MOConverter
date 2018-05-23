ed89207ae425c785315a5ee98b490a136790171d
Darko-Martinovic <darko.martinovic@outlook.com>
2018-05-23 10:03:20 +0200

    Decorative changes in the code.

--- Converter/Extension/Table extensions/TableExtension.cs
+++ Converter/Extension/Table extensions/TableExtension.cs
@@ -3,6 +3,7 @@ using Microsoft.SqlServer.Management.Smo;
 using System;
 using System.Diagnostics;
 using System.Linq;
+using static Converter.Options.Options;
 
 namespace Converter.Extension
 {
@@ -12,8 +13,8 @@ namespace Converter.Extension
 
         public static bool SwitchToMo(
                                       this Table self, 
-                                      Database InMemDatabase, 
-                                      Database Traditional, 
+                                      Database inMemDatabase, 
+                                      Database traditional, 
                                       Configuration.Configuration cnf, 
                                       Options.Options o, 
                                       ref string error, 
@@ -24,14 +25,14 @@ namespace Converter.Extension
             string schemaName = self.Schema;
             string dataBaseName = self.Parent.Name;
 
-            if (InMemDatabase.Schemas.Contains(schemaName) == false)
+            if (inMemDatabase.Schemas.Contains(schemaName) == false)
             {
-                Schema hr = new Schema(InMemDatabase, schemaName);
-                InMemDatabase.Schemas.Add(hr);
-                InMemDatabase.Schemas[schemaName].Create();
+                Schema hr = new Schema(inMemDatabase, schemaName);
+                inMemDatabase.Schemas.Add(hr);
+                inMemDatabase.Schemas[schemaName].Create();
             }
 
-            if (InMemDatabase.Tables.Contains(self.Name, schemaName))
+            if (inMemDatabase.Tables.Contains(self.Name, schemaName))
             {
                 logger.Log("\t" + "Already exists", self.FName());
                 return true;
@@ -41,23 +42,23 @@ namespace Converter.Extension
             bool hasIdentities = false;
 
 
-            Table newTable = new Table(InMemDatabase, self.Name, schemaName);
-
-            // default true
-            newTable.IsMemoryOptimized = true;
-            // default schema and data
-            newTable.Durability = DurabilityType.SchemaAndData;
+            Table newTable = new Table(inMemDatabase, self.Name, schemaName)
+            {
+                // default true
+                IsMemoryOptimized = true,
+                // default schema and data
+                Durability = DurabilityType.SchemaAndData
+            };
 
 
             // Add columns
-            Column newColumn = null;
             foreach (Column c in self.Columns)
             {
-                newColumn = new Column(newTable, c.Name);
+                var newColumn = new Column(newTable, c.Name);
 
                 newColumn.CopyPropertiesFrom(c);
 
-                SupportUnsupported(newColumn, c, Traditional, logger, ref error, ref hasIdentities,true);
+                SupportUnsupported(newColumn, c, traditional, logger, ref error, ref hasIdentities,true);
 
                 newTable.Columns.Add(newColumn);
 
@@ -72,24 +73,22 @@ namespace Converter.Extension
                 {
                     hasPrimaryKey = true;
                     Index idx = new Index(newTable, i.Name);
-                    if (o.UseHashIndexes == Options.Options.IndexDecision.Hash)
+                    if (o.UseHashIndexes == IndexDecision.Hash)
                     {
                         idx.IndexType = IndexType.NonClusteredHashIndex;
                         idx.BucketCount = self.RowCount == 0 ? 64 : (int)self.RowCount *2;
                     }
-                    else if ( o.UseHashIndexes == Options.Options.IndexDecision.Range)
-                    {
+                    else if (o.UseHashIndexes == IndexDecision.Range)
                         idx.IndexType = IndexType.NonClusteredIndex;
-                    }
                     else
                     {
                         if (self.ExtendedProperties[cnf.EPName] != null)
                         {
-                            string value = self.ExtendedProperties[cnf.EPName].Value.ToString().ToUpper();
+                            var value = self.ExtendedProperties[cnf.EPName].Value.ToString().ToUpper();
                             if (value == "HASH")
                             {
                                 idx.IndexType = IndexType.NonClusteredHashIndex;
-                                idx.BucketCount = self.RowCount == 0 ? 64 : (int)self.RowCount;
+                                idx.BucketCount = self.RowCount == 0 ? 64 : (int) self.RowCount;
                             }
                             else
                             {
@@ -100,14 +99,11 @@ namespace Converter.Extension
                         {
                             idx.IndexType = IndexType.NonClusteredIndex;
                         }
-
-                       
                     }
+
                     idx.IndexKeyType = IndexKeyType.DriPrimaryKey;
                     foreach (IndexedColumn ic in i.IndexedColumns)
-                    {
                         idx.IndexedColumns.Add(new IndexedColumn(idx, ic.Name));
-                    }
                     newTable.Indexes.Add(idx);
                 }
             }
@@ -116,22 +112,25 @@ namespace Converter.Extension
             foreach (Index i in self.Indexes)
             {
                 if (i.IndexKeyType == IndexKeyType.DriPrimaryKey)
-                {
                     continue;
-                }
-                else if (i.IndexType == IndexType.NonClusteredIndex)
+
+                if (i.IndexType == IndexType.NonClusteredIndex)
                 {
 
                     // Limit the total number of indexes to 8 for SQLServer2016
                     if (enumFeatures == SQLServerMoFeatures.SQLServer2016 && noIndexes == 8)
                     {
-                        logger.LogWarErr("Error:Create index failed", "Could not create in-memory index " + i.Name + " because it exceeds the maximum of 8 allowed per table or view.");
+                        logger.LogWarErr("Error:Create index failed", $"Could not create in-memory index {i.Name} " +
+                                                                      $"because it exceeds the maximum of 8 allowed per table or view.");
                         continue;
                     }
 
-                    Index idx = new Index(newTable, i.Name);
-                    idx.IndexType = IndexType.NonClusteredIndex;
-                    idx.IndexKeyType = IndexKeyType.None;
+                    Index idx = new Index(newTable, i.Name)
+                    {
+                        IndexType = IndexType.NonClusteredIndex,
+                        IndexKeyType = IndexKeyType.None
+                    };
+
                     bool hasColumns = false;
                     foreach (IndexedColumn ic in i.IndexedColumns)
                     {
@@ -140,7 +139,8 @@ namespace Converter.Extension
                         // nvarchar(max) is not allowed
                         if (newTable.Columns[ic.Name].DataType.MaximumLength == -1)
                         {
-                            logger.LogWarErr("Warning:Create index", "Could not include " + ic.Name + " in index " + idx.Name + " The column has nvarchar(max) type which is not allowed!");
+                            logger.LogWarErr("Warning:Create index",
+                                $"Could not include {ic.Name} in index {idx.Name} The column has nvarchar(max) type which is not allowed!");
                             continue;
                         }
                         idx.IndexedColumns.Add(new IndexedColumn(idx, ic.Name));
@@ -158,22 +158,24 @@ namespace Converter.Extension
 
             if (hasPrimaryKey == false)
             {
-                error = "Error:Table :" + self.FName() + " has no primary key";
+                error = $"Error:Table :{self.FName()} has no primary key";
                 //logger.LogWarErr(error, self.FName());
                 return false;
             }
 
-            //if (InMemDatabase.Tables.Contains(newTable.Name, schemaName))
-            //    InMemDatabase.Tables[newTable.Name, schemaName].Drop();
+            //if (inMemDatabase.Tables.Contains(newTable.Name, schemaName))
+            //    inMemDatabase.Tables[newTable.Name, schemaName].Drop();
 
             // Add checks
             foreach (Check ch in self.Checks)
             {
                 Check newch = new Check(newTable, ch.Name);
                 newch.CopyPropertiesFrom(ch);
-                if (newch.Text.ToLower().Contains("upper") || newch.Text.ToLower().Contains("like") || newch.Text.ToLower().Contains("charindex"))
+                if (newch.Text.ToLower().Contains("upper") || newch.Text.ToLower().Contains("like") ||
+                    newch.Text.ToLower().Contains("charindex"))
                 {
-                    logger.LogWarErr("Warning " + newch.Name, " can not apply constraint on table " + self.FName() + " because it contains forbidden functions ");
+                    logger.LogWarErr($"Warning {newch.Name}",
+                        $" can not apply constraint on table {self.FName()} because it contains forbidden functions ");
                     continue;
                 }
                 newTable.Checks.Add(newch);
@@ -182,8 +184,7 @@ namespace Converter.Extension
             // Skip triggers
             foreach (Trigger tr in self.Triggers)
             {
-                logger.LogWarErr("Warning " + tr.Name, " can not create trigger on table " + self.FName() + " Please, create trigger manually! ");
-                continue;
+                logger.LogWarErr($"Warning {tr.Name}", $" can not create trigger on table {self.FName()}.Please, create trigger manually! ");
             }
 
             try
@@ -207,25 +208,25 @@ namespace Converter.Extension
             // if copy data is checked
             if (o.CopyData)
             {
-                if (InMemDatabase.Tables.Contains(cnf.helperTableName, cnf.helperSchema))
-                    InMemDatabase.Tables[cnf.helperTableName, cnf.helperSchema].Drop();
+                if (inMemDatabase.Tables.Contains(cnf.helperTableName, cnf.helperSchema))
+                    inMemDatabase.Tables[cnf.helperTableName, cnf.helperSchema].Drop();
 
                 try
                 {
                     logger.Log("Insert data ", newTable.FName());
                     //Insert into 
-                    Traditional.ExecuteNonQuery(self.InsertIntoStm(InMemDatabase.Name, cnf.fullName));
+                    traditional.ExecuteNonQuery(self.InsertIntoStm(inMemDatabase.Name, cnf.fullName));
                     //Insert statement
-                    Table test = InMemDatabase.Tables[cnf.helperTableName, cnf.helperSchema];
-                    InMemDatabase.ExecuteNonQuery(newTable.FullInsertStm(test.SelectStm(), hasIdentities, cnf.fullName));
+                    Table test = inMemDatabase.Tables[cnf.helperTableName, cnf.helperSchema];
+                    inMemDatabase.ExecuteNonQuery(newTable.FullInsertStm(test.SelectStm(), hasIdentities, cnf.fullName));
                     retValue = true;
                     logger.Log("OK ", newTable.FName());
                     //
                 }
                 catch (Exception ex)
                 {
-                    if (InMemDatabase.Tables.Contains(newTable.Name, schemaName))
-                        InMemDatabase.Tables[newTable.Name, schemaName].Drop();
+                    if (inMemDatabase.Tables.Contains(newTable.Name, schemaName))
+                        inMemDatabase.Tables[newTable.Name, schemaName].Drop();
 
                     logger.Log("Error", self.FName());
                 
@@ -247,22 +248,24 @@ namespace Converter.Extension
             return retValue;
         }
 
-        public static void SupportUnsupported(Column newColumn, Column c, Database Traditional, ILog logger, ref string error, ref bool hasIdentities, bool isTable)
+        public static void SupportUnsupported(Column newColumn, Column c, Database traditional, ILog logger, ref string error, ref bool hasIdentities, bool isTable)
         {
             if (c.DataType.SqlDataType == SqlDataType.UserDefinedDataType)
             {
-                UserDefinedDataType ud = Traditional.UserDefinedDataTypes[c.DataType.Name, c.DataType.Schema];
+                UserDefinedDataType ud = traditional.UserDefinedDataTypes[c.DataType.Name, c.DataType.Schema];
                 try
                 {
-                    newColumn.DataType = new DataType();
-                    newColumn.DataType.SqlDataType = Uddt.Type.DetermineSqlDataType(ud.SystemType, ref error);
-                    newColumn.DataType.MaximumLength = c.DataType.MaximumLength;
-                    newColumn.DataType.NumericScale = c.DataType.NumericScale;
-                    newColumn.DataType.NumericPrecision = c.DataType.NumericPrecision;
+                    newColumn.DataType = new DataType
+                    {
+                        SqlDataType = Uddt.Type.DetermineSqlDataType(ud.SystemType, ref error),
+                        MaximumLength = c.DataType.MaximumLength,
+                        NumericScale = c.DataType.NumericScale,
+                        NumericPrecision = c.DataType.NumericPrecision
+                    };
                     newColumn.Nullable = c.Nullable;
                     newColumn.Default = c.Default;
                     logger.LogWarErr("Warning " + (isTable == true ? c.FName() : c.UDTName()),
-                                      "Convertion datatype : " + ud.Name + " to " + newColumn.DataType.SqlDataType.ToString());
+                                      $"Convertion datatype : {ud.Name}  to  {newColumn.DataType.SqlDataType.ToString()}");
                 }
                 catch (Exception ex)
                 {
@@ -277,7 +280,8 @@ namespace Converter.Extension
                 }
             }
             // support for CLR types
-            else if (c.DataType.SqlDataType == SqlDataType.HierarchyId || c.DataType.SqlDataType == SqlDataType.Geography || c.DataType.SqlDataType == SqlDataType.Geometry)
+            else if (c.DataType.SqlDataType == SqlDataType.HierarchyId ||
+                     c.DataType.SqlDataType == SqlDataType.Geography || c.DataType.SqlDataType == SqlDataType.Geometry)
             {
                 newColumn.DataType.SqlDataType = SqlDataType.NVarChar;
                 if (c.DataType.SqlDataType == SqlDataType.HierarchyId)
@@ -285,36 +289,37 @@ namespace Converter.Extension
                 else
                     newColumn.DataType.MaximumLength = -1;
 
-                logger.LogWarErr("Warning " + (isTable == true ? c.FName() : c.UDTName()),
-                                  "Convertion CLR datatype to " + newColumn.DataType.SqlDataType.ToString());
+                logger.LogWarErr("Warning " + (isTable ? c.FName() : c.UDTName()),
+                    $"Convertion CLR datatype to {newColumn.DataType.SqlDataType.ToString()}");
             }
             // support for XML type
             else if (c.DataType.SqlDataType == SqlDataType.Xml)
             {
                 newColumn.DataType.SqlDataType = SqlDataType.NVarChar;
                 newColumn.DataType.MaximumLength = -1;
-                logger.LogWarErr("Warning " + (isTable == true ? c.FName() : c.UDTName()),
-                                 "Convertion XML datatype to " + newColumn.DataType.SqlDataType.ToString());
+                logger.LogWarErr("Warning " + (isTable ? c.FName() : c.UDTName()),
+                                 $"Convertion XML datatype to  {newColumn.DataType.SqlDataType.ToString()}");
             }
-            else if (c.DataType.SqlDataType == SqlDataType.Variant || c.DataType.SqlDataType == SqlDataType.Text || c.DataType.SqlDataType == SqlDataType.NText)
+            else if (c.DataType.SqlDataType == SqlDataType.Variant || c.DataType.SqlDataType == SqlDataType.Text ||
+                     c.DataType.SqlDataType == SqlDataType.NText)
             {
                 newColumn.DataType.SqlDataType = SqlDataType.NVarChar;
                 newColumn.DataType.MaximumLength = -1;
-                logger.LogWarErr("Warning " + (isTable == true ? c.FName() : c.UDTName()),
-                                 "Convertion " + c.DataType.SqlDataType + " TO " + newColumn.DataType.SqlDataType.ToString());
+                logger.LogWarErr("Warning " + (isTable  ? c.FName() : c.UDTName()),
+                                 "Convertion " + c.DataType.SqlDataType + " TO " + newColumn.DataType.SqlDataType);
             }
             else if (c.DataType.SqlDataType == SqlDataType.Image)
             {
                 newColumn.DataType.SqlDataType = SqlDataType.VarBinaryMax;
                 newColumn.DataType.MaximumLength = -1;
-                logger.LogWarErr("Warning " + (isTable == true ? c.FName() : c.UDTName()),
-                                 "Convertion " + c.DataType.SqlDataType + " TO " + newColumn.DataType.SqlDataType.ToString());
+                logger.LogWarErr("Warning " + (isTable ? c.FName() : c.UDTName()),
+                                 "Convertion " + c.DataType.SqlDataType + " TO " + newColumn.DataType.SqlDataType);
             }
             else if (c.DataType.SqlDataType == SqlDataType.DateTimeOffset)
             {
                 newColumn.DataType.SqlDataType = SqlDataType.DateTime2;
-                logger.LogWarErr("Warning " + (isTable == true ? c.FName() : c.UDTName()),
-                                 "Convertion " + c.DataType.SqlDataType + " TO " + newColumn.DataType.SqlDataType.ToString());
+                logger.LogWarErr("Warning " + (isTable  ? c.FName() : c.UDTName()),
+                                 "Convertion " + c.DataType.SqlDataType + " TO " + newColumn.DataType.SqlDataType);
             }
             //else
             //{
@@ -326,8 +331,8 @@ namespace Converter.Extension
                 newColumn.Computed = false;
                 newColumn.ComputedText = String.Empty;
                 //newColumn.IsPersisted = c.IsPersisted;
-                logger.LogWarErr("Warning " + (isTable == true ? c.FName() : c.UDTName()),
-                                 "Can not apply computed column " + c.ComputedText);
+                logger.LogWarErr("Warning " + (isTable ? c.FName() : c.UDTName()),
+                                 $"Can not apply computed column {c.ComputedText}");
             }
 
             //newColumn.Collation = c.Collation;
@@ -338,14 +343,14 @@ namespace Converter.Extension
                 hasIdentities = true;
                 // has to be 1
                 if (c.IdentityIncrement != 1 || c.IdentitySeed != 1)
-                    logger.LogWarErr("Warning " + (isTable == true ? newColumn.FName() : newColumn.UDTName()),
+                    logger.LogWarErr("Warning " + (isTable ? newColumn.FName() : newColumn.UDTName()),
                                     " setting identity seed and identity increment to 1 ");
                 newColumn.IdentityIncrement = 1;
                 newColumn.IdentitySeed = 1;
 
 
             }
-            return;
+
 
         }
 
